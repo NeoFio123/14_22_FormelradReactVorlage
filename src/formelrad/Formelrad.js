6d40a8cbcd1b12d3c758d0948b74f243f0966c8d
Neo Stutz <neo.stutz@bbcag.ch>
2025-06-11 22:45:17 +0200

    Fix merge conflict: Combined CalcU and CalcI features

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,7 +1,8 @@
 import {useState} from "react";
-import '../css/mvp.css';
-import formelrad from "../image/formelradelektronik.gif";
 import InputField from "../formular/InputField";
+import OutputField from "../formular/OutputField";
+import Checkbox from "../formular/Checkbox";
+import '../css/mvp.css';
 
 export default function Formelrad() {
     const [values, setValues] = useState({
@@ -11,35 +12,67 @@ export default function Formelrad() {
         p: ""
     })
 
+    // KOMBINIERTE LÖSUNG: Beide Features zusammengeführt
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("handleSubmit")
+        
+        // CalcU: Berechne u und i
         if (values.u === "" && values.i === "") {
             /*calculate u and i */
             setValues(values => ({...values, u: Math.sqrt(values.p * values.r)}));
             setValues(values => ({...values, i: Math.sqrt(values.p / values.r)}));
-        } else if (values.u === "" && values.r === "") {
+        } 
+        // CalcU: Berechne u und r  
+        else if (values.u === "" && values.r === "") {
             /*calculate u and r */
             setValues(values => ({...values, u: values.p / values.i}));
-            setValues(values => ({...values, r: values.p / values.i / values.i}));
+            setValues(values => ({...values, r: values.p / (values.i * values.i)}));
+        }
+        // CalcI: Berechne i und r
+        else if (values.i === "" && values.r === "") {
+            /*calculate i and r */
+            setValues(values => ({...values, i: values.p / values.u}));
+            setValues(values => ({...values, r: values.u * values.u / values.p}));
+        } 
+        // CalcI: Berechne i und p
+        else if (values.i === "" && values.p === "") {
+            /*calculate i and p */
+            setValues(values => ({...values, i: values.u / values.r}));
+            setValues(values => ({...values, p: values.u * values.u / values.r}));
         }
     }
 
     return (
-        <>
-            <section>
-                <header>
-                    <h2>Formelrad</h2>
-                    <img src={formelrad} width="200" alt="Formelrad"/>
-                </header>
-                <form onSubmit={handleSubmit}>
-                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
-                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
-                    <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
-                    <button type="submit">Calculate</button>
-                </form>
-            </section>
-        </>
-    )
+        <main>
+            <form onSubmit={handleSubmit}>
+                <table>
+                    <caption>Ohmsches Gesetz</caption>
+                    <tbody>
+                        <tr>
+                            <td><label htmlFor="u">Spannung in Volt:</label></td>
+                            <td><InputField name="u" value={values.u} setValue={setValues}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="i">Strom in Ampere:</label></td>
+                            <td><InputField name="i" value={values.i} setValue={setValues}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="r">Widerstand in Ohm:</label></td>
+                            <td><InputField name="r" value={values.r} setValue={setValues}/></td>
+                        </tr>
+                        <tr>
+                            <td><label htmlFor="p">Leistung in Watt:</label></td>
+                            <td><InputField name="p" value={values.p} setValue={setValues}/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2">
+                                <button type="submit">Berechnen</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </main>
+    );
 }
